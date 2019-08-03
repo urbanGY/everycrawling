438cbcd024a1b56f3cd6d5125b53be0ec50224ac
Hyun Seok Kim <sfsfkj@gmail.com>
2019-08-03 21:39:01 +0900

    new

---
+++ celery/task.py
@@ -0,0 +1,13 @@
+from setting import app
+#from . import link_crawling
+
+@app.task
+def say_hello():
+    print('helllo !')
+
+@app.task
+def update_data():
+    # movie_list = link_crawling.read_movie_list_test()
+    # site_list = link_crawling.read_site_list()
+    # result = link_crawling.body(site_list, movie_list)
+    print('result')
