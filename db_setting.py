bd509118de261f6ee42fd1ac09411ae61b4e6c8f
Hyun Seok Kim <34535994+urbanGY@users.noreply.github.com>
2019-08-08 22:43:58 +0900

    modulize

--- db_setting.py
+++ db_setting.py
@@ -1,12 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-engine = create_engine('postgresql://superson:superson@superson:5432/crawling', echo=True)
+
 Base = declarative_base()
 
-Session = sessionmaker(engine)
-db_session = Session()
+def get_rawdata_engine():
+    engine = create_engine('postgresql://superson:superson@superson:5432/crawling', echo=True)
+    return engine
 
-def init_db():
+def get_refineddata_engine():
+    engine = create_engine('postgresql://superson:superson@superson:5432/Everyreview', echo=True)
+    return engine
+
+def get_sesstion(engine):
+    Session = sessionmaker(engine)
+    db_session = Session()
+    return db_session
+
+def create_rawdata_table(engine):
     import db.models
     Base.metadata.create_all(engine)
